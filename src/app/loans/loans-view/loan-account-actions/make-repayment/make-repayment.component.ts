934b26406fac524e6cb8749f2ce78e38038fdbd7
Mayank Gupta <mayankgupta@indiagold.co>
2023-04-10 11:09:13 +0400

    repayment till today enabled

--- src/app/loans/loans-view/loan-account-actions/make-repayment/make-repayment.component.ts
+++ src/app/loans/loans-view/loan-account-actions/make-repayment/make-repayment.component.ts
@@ -89,6 +89,10 @@ export class MakeRepaymentComponent implements OnInit {
     this.paymentTypes = this.dataObject.paymentTypeOptions;
     this.paymentCommandTypes = [
       {
+        "id": "repayment",
+        "name": "Repayment till today",
+      },
+      {
         "id": "repayment-due-date",
         "name": "Repayment till due date"
       },
@@ -99,10 +103,6 @@ export class MakeRepaymentComponent implements OnInit {
       {
         "id": "principal-waive-off-closure",
         "name": "Principal waive off for closure"
-      },
-      {
-        "id": "repayment",
-        "name": "Repayment till today (disabled)",
       },
       {
         "id": "foreclosure",
